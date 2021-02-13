44bdc05f4fbb44c3808acc5343f443e31d5ceb5f
vasylpb <vasylpb89@gmail.com>
2021-02-13 21:20:14 +0200

    Authentication Strategies

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -1,9 +1,10 @@
 import express from 'express';
+import { currentUser } from '../middlewares/current-user';
 
 const router = express.Router()
 
-router.get('/api/users/currentUser', (req, res) => {
-    res.send('Hi there!')
+router.get('/api/users/currentUser', currentUser, (req, res) => {
+  res.send({ currentUser: req.currentUser || null })
 });
 
 export { router as currentUserRouter }
