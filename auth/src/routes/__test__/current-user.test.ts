9b07d3efd7eef0f89140b71f62ff86ddb472462d
vasylpb <vasylpb89@gmail.com>
2021-02-18 21:18:16 +0200

    testing isolated microservices

---
+++ auth/src/routes/__test__/current-user.test.ts
@@ -0,0 +1,24 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+it('responds with details about the current user', async () => {
+
+  const cookie = await global.getAuthCookie();
+
+  const response = await request(app)
+    .get('/api/users/currentUser')
+    .set('Cookie', cookie)
+    .send()
+    .expect(200);
+
+    expect(response.body.currentUser.email).toEqual('test@test.com');
+  });
+
+  it('responds with null of not authenticated', async () => {
+    const response = await request(app)
+      .get('/api/users/currentUser')
+      .send()
+      .expect(200);
+
+    expect(response.body.currentUser).toEqual(null);
+  });
