35126bc120e7965bfc2c2ed06b92d4bbaeedd28c
vasylpb <vasylpb89@gmail.com>
2021-02-25 20:39:36 +0200

    Code sharing library

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -7,8 +7,7 @@ import { currentUserRouter } from './routes/current-user'
 import { signinRouter } from './routes/signin'
 import { signoutRouter } from './routes/signout'
 import { signupRouter } from './routes/signup'
-import { errorHandler } from './middlewares/error-handler'
-import { NotFoundError } from './errors/not-found'
+import { errorHandler, NotFoundError } from '@sgtickets/common'
 
 const app = express()
 // for ingress nginx setup
--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -4,8 +4,7 @@ import jwt from 'jsonwebtoken';
 
 import { User } from '../models/User';
 import { Password } from '../services/password';
-import { validateRequest } from '../middlewares/validate-request';
-import { BadRequestError } from '../errors/bad-request-error';
+import { validateRequest, BadRequestError } from '@sgtickets/common';
 
 const router = express.Router();
 
