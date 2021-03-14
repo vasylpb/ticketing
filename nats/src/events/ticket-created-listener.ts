0ef1428be45948090d72a0cf95f6f0cf2cf7c96f
vasylpb <vasylpb89@gmail.com>
2021-03-14 16:53:15 +0200

    Connecting to NATS in Node

---
+++ nats/src/events/ticket-created-listener.ts
@@ -0,0 +1,12 @@
+import { Message } from 'node-nats-streaming';
+import { Listener, TicketCreatedEvent, Subjects } from '@sgtickets/common';
+
+export class TicketCreatedListener extends Listener<TicketCreatedEvent> {
+  readonly subject = Subjects.TicketCreated;
+  queueGroupName = 'payments-service';
+
+  onMessage(data: TicketCreatedEvent['data'], msg: Message) {
+    console.log('Event data!', data);
+    msg.ack();
+  }
+}
---
+++ nats/src/publisher.ts
@@ -0,0 +1,23 @@
+import nats from 'node-nats-streaming';
+import { TicketCreatedPublisher } from './events/ticket-created-publisher';
+
+console.clear();
+
+const stan = nats.connect('ticketing', 'abc', {
+  url: 'http://localhost:4222'
+});
+
+stan.on('connect', async () => {
+  console.log('Publisher connected to NATS');
+
+  const publisher = new TicketCreatedPublisher(stan);
+
+  await publisher.publish({
+    id: '123',
+    title: 'basketball',
+    price: 20,
+    userId: 'qqq',
+    version: 0
+  })
+
+});
